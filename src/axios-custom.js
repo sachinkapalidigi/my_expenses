44e6590b30c68cd242ec03bb54d1efec6ef333cc
sachin <sachinkapalidigi@gmail.com>
2020-04-07 19:40:18 +0530

    added landing page images and set up env for prod

--- src/axios-custom.js
+++ src/axios-custom.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import store from './redux/store';
 
+let baseURL = 'http://52.14.67.195:8080/';
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:8080/';
+}
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL,
 });
 
 export const authInstance = () => {
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import img from '../../assets/images/landingpage.jpg';
 
-const LandingPage = () => <div>Landing Page</div>;
+const LandingPage = () => {
+  return (
+    <Row justify="center">
+      <Col xs={24} sm={24} md={16} lg={16} xl={12}>
+        <h2 style={{ textAlign: 'center' }}>Manage Your Expenses Here!</h2>
+        <img
+          style={{ width: '100%', height: '100%' }}
+          src={img}
+          alt="Manage your expenses"
+        />
+      </Col>
+    </Row>
+  );
+};
 
 export default LandingPage;
